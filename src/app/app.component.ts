60e6e5e301a095d894230bd5af3b221f8f575104
srihari <srihari24680@gmail.com>
2025-02-04 21:23:06 +0530

    used for configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, RouterLink } from '@angular/router';
+import { AddEntriesComponent } from './add-entries/add-entries.component';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { ViewEntriesComponent } from './view-entries/view-entries.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink, AddEntriesComponent, AnalysisComponent, ViewEntriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'angulartailwind';
-}
+}) 
+// @Component({
+//   selector: 'app-root',
+//   standalone: true, // Mark as standalone
+//   template: `
+//     <router-outlet></router-outlet>
+    
+//   `,
+//   imports: [AddEntriesComponent, RouterOutlet, ViewEntriesComponent, AnalysisComponent] // Add the standalone component to imports
+// })
+  export class AppComponent {
+   title = 'angulartailwind';
+  //  this.router.navigateByUrl('/user');
+ }
+
+
