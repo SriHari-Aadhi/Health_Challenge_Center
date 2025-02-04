75de1e55829f807e7faf4da2889a415e6f6e460b
srihari <srihari24680@gmail.com>
2025-02-04 21:08:53 +0530

    Added AnalysisComponent to show the workout analysis in charts from the workoutDataFile on LocalStorage,the work still in progress

---
+++ src/app/analysis/analysis.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+Chart.register()
+@Component({
+  selector: 'app-analysis',
+  standalone: true,
+  imports: [],
+  templateUrl: './analysis.component.html',
+  styleUrl: './analysis.component.css'
+})
+export class AnalysisComponent {
+
+
+}
