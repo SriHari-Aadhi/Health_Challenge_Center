255f994dbe42b989ae8f3b773f87e7358e20a796
srihari <srihari24680@gmail.com>
2025-02-04 21:12:17 +0530

    the paths are configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddEntriesComponent } from './add-entries/add-entries.component';
+import { ViewEntriesComponent } from './view-entries/view-entries.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'add', component: AddEntriesComponent},
+    {path: 'view', component: ViewEntriesComponent},
+    {path: 'analysis', component: AnalysisComponent},
+    { path: '', redirectTo: '/view', pathMatch: 'full' }
+  ];
